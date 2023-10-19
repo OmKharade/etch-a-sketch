d3125f177cb1598cbbe01e9efdaaeec9077b514c
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-10-19 18:10:04 -0400

    added buttons : grid-lines & erase

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # etch-a-sketch
+
+## Progress Check-up
+
+I made a tasklist for everytime I worked on this project. The goal of it was to track my progress as I learn more concepts and apply them to make a fleshed out project. 
+- - 
+These are my check-ins:
+
+- (10/18) Code for the canvas itself
+    1. create a 16x16 grid 
+    2. add .grid-square as child of .grid
+    3. onmousedown -> background-color of .grid-square = black
+
+- (10/19) Added buttons
+    1. show-grid - .toggle the gap on grid
+    2. erase - background-color of .grid-square = background
+    3. add theme for toggled button / hover
+    4. polish css
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /* task list :
-1. create 16x16 grid 
-2. add .grid-square as child to .grid
-3. onmousedown -> background-color of .grid-square = black
+1. configure buttons
+    1. show-grid
+    2. erase
+2. add hover and status effects
+3. fix css
 */
 
 // select .grid
@@ -21,13 +23,34 @@ const gridSquares = document.querySelectorAll('.grid-square')
 // on mouse enter on any square -> change background
 for(let i = 0; i < 16*16; i++){
     gridSquares[i].addEventListener('mousedown', e => {
-        gridSquares[i].style.backgroundColor = 'black'
-        console.log(i);
+        if(isEraser) // erase mode
+            gridSquares[i].style.backgroundColor = 'var(--secondary)'
+        else // ink mode
+            gridSquares[i].style.backgroundColor = 'black'
     })
     gridSquares[i].addEventListener('mouseenter', e => {
         if(e.buttons >0){ // if mouse is pressed
-        gridSquares[i].style.backgroundColor = 'black'
-        console.log(i);
+            if(isEraser) // erase mode
+                gridSquares[i].style.backgroundColor = 'var(--secondary)'
+            else // ink mode
+                gridSquares[i].style.backgroundColor = 'black'
         }
     })
 }
+
+// toggle buttons
+
+const toggleGridLines = document.querySelector('#toggle-grid-lines')
+toggleGridLines.addEventListener('click',()=>{
+    gridContainer.classList.toggle('grid-lines') // grid lines on or off
+    toggleGridLines.classList.toggle('toolbarBtnToggled') // change button color
+})
+
+const toggleEraser = document.querySelector('#toggle-eraser')
+let isEraser = false // eraser is off by default
+
+toggleEraser.addEventListener('click',()=>{
+    isEraser = isEraser? false:true; // change eraser value (false <--> true)
+    toggleEraser.classList.toggle('toolbarBtnToggled') // change button color
+})
+
